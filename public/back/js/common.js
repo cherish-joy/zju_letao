0abb4df9627ad8ab8d90de5c09430103c6587888
joy <cherish_shy@163.com>
2019-04-08 23:07:53 +0800

    首页完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,10 +1,55 @@
 // ajax全局事件
 // ajaxComplete 当每个ajax请求完成的时候 调用 不管成功或者失败
 // ajaxError
-
+// 登录拦截
 $(document).ajaxStart(function () {
     NProgress.start();
-})
+});
 $(document).ajaxStop(function () {
     NProgress.done();
+});
+$(function () {
+    if (location.href.indexOf("login")== -1) {
+        $.ajax({
+            url: " /employee/checkRootLogin",
+            type: "get",
+            dataType: "json",
+            success:function (info) {
+                if (info.success){
+                    console.log("登录成功");
+                }
+                if (info.error === 400) {
+                    location.href = "login.html"
+                }
+            }
+        });
+    }
 })
+
+
+$(function() {
+    $(".list-type").click(function () {
+        $(".child").slideToggle();
+    })
+    $(".icon-left").click(function () {
+        $(".left").toggleClass("hiddenmenu");
+        $(".main").toggleClass("hiddenmenu");
+        $(".main-top").toggleClass("hiddenmenu")
+    })
+    $(".icon-right").click(function () {
+        $(".modal").modal("show");
+    })
+    $("#logout-btn").click(function () {
+        $.ajax({
+            url:"  /employee/employeeLogout",
+            type:"get",
+            dataType:"json",
+            success:function (info) {
+                if (info.success) {
+                    location.href = "login.html"
+                }
+            }
+        })
+    })
+})
+
